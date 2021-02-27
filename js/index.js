53183ffd0213da7c081f7a6c527629896703f7c3
Bernat Jorquera <bernatjv19@gmail.com>
2021-02-27 19:47:01 +0100

    Las tarjetas enseñan la info más básica de cada personaje

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const creaPersonajes = personajes => {
+  for (const personaje of personajes) {
+    const personajeDummy = document.querySelector(".personaje-dummy");
+    const listaPersonajes = document.querySelector(".personajes");
+    const nuevoPersonaje = personajeDummy.cloneNode(true);
+    clasePersonaje = personaje.nombre.replace(" ", "");
+    nuevoPersonaje.className = nuevoPersonaje.className
+      .replace("personaje-dummy", clasePersonaje);
+    listaPersonajes.append(nuevoPersonaje);
+    const personajeNombre = document.querySelector(`.${clasePersonaje} .nombre`);
+    personajeNombre.textContent = personaje.nombre;
+    const personajeEdad = document.querySelector(`.${clasePersonaje} .info .metadata`).children[0];
+    personajeEdad.textContent = personajeEdad.textContent.replace("X", personaje.edad);
+  }
+}
+
+creaPersonajes(personajesGot);
