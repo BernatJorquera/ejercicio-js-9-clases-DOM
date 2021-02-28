a3a5779ce14616e054387ccd6b810488cfc5c549
Bernat Jorquera <bernatjv19@gmail.com>
2021-03-01 00:35:52 +0100

    "Emojis" agregados

--- js/index.js
+++ js/index.js
@@ -2,17 +2,12 @@ const creaPersonajes = personajes => {
   const personajeDummy = document.querySelector(".personaje-dummy");
   const listaPersonajes = document.querySelector(".personajes");
   for (const personaje of personajes) {
-    console.log(personaje.nombre);
     const nuevoPersonaje = personajeDummy.cloneNode(true);
 
-    clasePersonaje = personaje.nombre.replace(" ", "");
+    const clasePersonaje = personaje.nombre.replace(" ", "");
     const personajeFoto = nuevoPersonaje.querySelector(".nueva-imagen");
     let nombrePropio = personaje.nombre.split(" ")[0].toLowerCase();
-
-    console.log(nombrePropio)
     personajeFoto.src = `img/${nombrePropio}.jpg`;
-
-
     nuevoPersonaje.className = nuevoPersonaje.className
       .replace("personaje-dummy", clasePersonaje);
     listaPersonajes.append(nuevoPersonaje);
@@ -24,6 +19,15 @@ const creaPersonajes = personajes => {
       nuevoPersonaje.querySelector(`.estado .muerto`).classList.add("d-none");
     } else if (personaje.estado === "Muerto") {
       nuevoPersonaje.querySelector(`.estado .vivo`).classList.add("d-none");
+    }
+    if (personaje.constructor.name === "Rey") {
+      nuevoPersonaje.querySelector(`.emoji`).textContent = "Corona";
+    } else if (personaje.constructor.name === "Luchador") {
+      nuevoPersonaje.querySelector(`.emoji`).textContent = "Espada";
+    } else if (personaje.constructor.name === "Asesor") {
+      nuevoPersonaje.querySelector(`.emoji`).textContent = "Birrete";
+    } else if (personaje.constructor.name === "Escudero") {
+      nuevoPersonaje.querySelector(`.emoji`).textContent = "Escudo";
     }
     for (const childNode of nuevoPersonaje.querySelector(".info-especifica").children) {
       LoopPropiedades:
